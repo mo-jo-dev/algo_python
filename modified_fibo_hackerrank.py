37de19e745bfc201fa687a995be6587426a1233a
mo-jo-dev <b210055@nitsikkim.ac.in>
2024-06-08 00:55:50 +0530

    added modified fibo hackerrank problem solution

---
+++ modified_fibo_hackerrank.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'fibonacciModified' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER t1
+#  2. INTEGER t2
+#  3. INTEGER n
+#
+memo = {}
+def fibonacciModified(t1, t2, n):
+    if n in memo:
+        return memo[n]
+    if n == 1:
+        return t1
+    if n == 2:
+        return t2
+    else:
+        res = fibonacciModified(t1, t2, n-1)*fibonacciModified(t1, t2, n-1) + fibonacciModified(t1, t2, n-2)
+    memo[n] = res
+    return memo[n]
+    
+
+    
+
+
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    t1 = int(first_multiple_input[0])
+
+    t2 = int(first_multiple_input[1])
+
+    n = int(first_multiple_input[2])
+
+    result = fibonacciModified(t1, t2, n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
